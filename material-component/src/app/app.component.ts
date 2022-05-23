828e0a26bdcc85993ad887f78da1fbefcfa63998
F3us2022 <arsenal0288@gmail.com>
2022-05-23 08:47:55 -0400

    V1.1 commit

--- material-component/src/app/app.component.ts
+++ material-component/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-component';
+
+  //Progress spinner values
   badgeNum: number = 7;
+  color: ThemePalette = 'primary';
+  mode: ProgressSpinnerMode = 'determinate';
+  value = 50;
+  spinner = false;
+  loadBtn() {
+    this.spinner = true;
+    setTimeout(() => (this.spinner = false), 5000);
+  }
 }
--- material-component/src/app/material/material.module.ts
+++ material-component/src/app/material/material.module.ts
@@ -3,12 +3,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
 import { MatBadgeModule } from '@angular/material/badge';
+import {
+  MatProgressSpinnerModule,
+  ProgressSpinnerMode,
+} from '@angular/material/progress-spinner';
+import { ThemePalette } from '@angular/material/core';
 
 const MaterialComponent = [
   MatButtonModule,
   MatButtonToggleModule,
   MatIconModule,
   MatBadgeModule,
+  MatProgressSpinnerModule,
 ];
 
 @NgModule({
