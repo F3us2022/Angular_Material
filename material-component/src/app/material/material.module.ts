ca6fb7b8a1e73facb09e6a1e9373e546d55433bd
F3us2022 <arsenal0288@gmail.com>
2022-05-23 04:31:54 -0400

    initial commit

---
+++ material-component/src/app/material/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+const MaterialComponent = [MatButtonModule];
+
+@NgModule({
+  imports: [MaterialComponent],
+  exports: [MaterialComponent],
+})
+export class MaterialModule {}
