c6f350c41aee7e18db7fdfe48e6a7b7be21af201
F3us2022 <arsenal0288@gmail.com>
2022-05-23 07:27:11 -0400

    V1.1 commit

--- material-component/src/app/material/material.module.ts
+++ material-component/src/app/material/material.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-const MaterialComponent = [MatButtonModule, MatButtonToggleModule];
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+
+const MaterialComponent = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatBadgeModule,
+];
 
 @NgModule({
-  imports: [MaterialComponent, MatButtonToggleModule],
-  exports: [MaterialComponent, MatButtonToggleModule],
+  imports: [MaterialComponent],
+  exports: [MaterialComponent],
 })
 export class MaterialModule {}
